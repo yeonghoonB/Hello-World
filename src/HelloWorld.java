3bf488317cd499c008653a5f37c66f3e8a80cc4c
yeonghoon <yeonghoon@DESKTOP-61P65RC>
2021-08-31 16:42:28 +0900

    hi github 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     public static void main(String[] arg) {
         System.out.println("Hello World");
         System.out.println("hi YeongHoon");
+        System.out.println("hi github");
     }
 }
